8ea55b95935a021d0e8a902534f25b505c8821c8
F4uzan <derpish123@gmail.com>
2016-07-05 09:24:03 +0700

    3.10.96

--- sound/core/hrtimer.c
+++ sound/core/hrtimer.c
@@ -88,7 +88,6 @@ static int snd_hrtimer_close(struct snd_timer *t)
 static int snd_hrtimer_start(struct snd_timer *t)
 {
 	struct snd_hrtimer *stime = t->private_data;
-
 	atomic_set(&stime->running, 0);
 	hrtimer_try_to_cancel(&stime->hrt);
 	hrtimer_start(&stime->hrt, ns_to_ktime(t->sticks * resolution),
